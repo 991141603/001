7f361bba1d3de35190c195d10edfb9bdb9646b43
brain <991141603@qq.com>
2019-09-09 18:15:13 +0800

    002

--- test1/src/main/java/com/itheima/controller/Demo01.java
+++ test1/src/main/java/com/itheima/controller/Demo01.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Demo01 {
     @RequestMapping("/show")
     public String show(){
+        System.out.println("中国历史悠久");
         return "hello springboot";
     }
 }
