f23a5b28e1ac09c4058be27c2dbec730feefa71e
brain <991141603@qq.com>
2019-09-09 18:29:33 +0800

    004

--- test1/src/main/java/com/itheima/controller/Demo01.java
+++ test1/src/main/java/com/itheima/controller/Demo01.java
@@ -8,6 +8,9 @@ public class Demo01 {
     @RequestMapping("/show")
     public String show(){
         System.out.println("卑鄙是卑鄙者的通行证");
+        System.out.println("卑鄙是卑鄙者的通行证");
+        System.out.println("卑鄙是卑鄙者的通行证");
+        System.out.println("卑鄙是卑鄙者的通行证");
         return "hello springboot";
     }
 }
